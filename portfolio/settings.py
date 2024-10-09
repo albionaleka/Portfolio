21b1492327faac9e9e01f8c70d7c288e656993aa
Albiona <albionnalekaa@gmail.com>
2024-10-09 19:06:18 +0200

    vercel configuration

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@ufugz#^lw*6+j@12-y=ng%%g^mv*=7bm=p_a@$yc0j+9fb^=3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost', '.me']
 
 
 # Application definition
